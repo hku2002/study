0a49d4fcc7d83bbb8477c958d57f84ecfa026d0e
Hwang <hku2002@hanmail.net>
2023-04-16 18:52:53 +0900

    refactor: 불필요한 public 제거

--- spring-core/src/test/java/com/study/core/order/OrderServiceTest.java
+++ spring-core/src/test/java/com/study/core/order/OrderServiceTest.java
@@ -7,7 +7,7 @@ import com.study.core.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class OrderServiceTest {
+class OrderServiceTest {
 
     MemberService memberService = new MemberServiceImpl();
     OrderService orderService = new OrderServiceImpl();
