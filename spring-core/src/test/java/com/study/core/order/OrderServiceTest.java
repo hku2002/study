c5cf22c7c9c46ddef6fdfa8960e5cbacc2a8c769
Hwang <hku2002@hanmail.net>
2023-04-16 16:09:50 +0900

    test: 주문 테스트 코드 최초 추가

---
+++ spring-core/src/test/java/com/study/core/order/OrderServiceTest.java
@@ -0,0 +1,28 @@
+package com.study.core.order;
+
+import com.study.core.member.Grade;
+import com.study.core.member.Member;
+import com.study.core.member.MemberService;
+import com.study.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void order() {
+        // given
+        Long memberId = 1L;
+        Member member = new Member(memberId, "홍길동", Grade.VIP);
+
+        // when
+        memberService.join(member);
+        Order order = orderService.createOrder(1L, "주문 테스트 item", 10000);
+
+        // then
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
