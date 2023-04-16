229a6bc6ebc16d7f476e9dddc4270654597afa82
Hwang <hku2002@hanmail.net>
2023-04-16 15:48:47 +0900

    refactor: 파일명 변경

--- spring-core/src/main/java/com/study/core/member/MemberServiceImpl.java
+++ spring-core/src/main/java/com/study/core/member/MemberServiceImpl.java
@@ -2,7 +2,7 @@ package com.study.core.member;
 
 public class MemberServiceImpl implements MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepositoryImpl();
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
     @Override
     public void join(Member member) {
         memberRepository.save(member);
