9f519440173ebbed0654dbde956dcbe13b3490f2
Hwang <hku2002@hanmail.net>
2023-04-18 23:53:00 +0900

    feat: 롬복 추가

--- spring-core/src/main/java/com/study/core/order/OrderServiceImpl.java
+++ spring-core/src/main/java/com/study/core/order/OrderServiceImpl.java
@@ -3,20 +3,15 @@ package com.study.core.order;
 import com.study.core.discount.DiscountPolicy;
 import com.study.core.member.Member;
 import com.study.core.member.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
