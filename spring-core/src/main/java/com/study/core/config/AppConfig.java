4af3527ffb6fdbfaf4905b1abccb0ebb7f883c1c
Hwang <hku2002@hanmail.net>
2023-04-22 13:27:53 +0900

    fix: 중복된 빈 삭제

--- spring-core/src/main/java/com/study/core/config/AppConfig.java
+++ spring-core/src/main/java/com/study/core/config/AppConfig.java
@@ -1,35 +1,4 @@
 package com.study.core.config;
 
-import com.study.core.discount.DiscountPolicy;
-import com.study.core.discount.FixDiscountPolicy;
-import com.study.core.member.MemberService;
-import com.study.core.member.MemberServiceImpl;
-import com.study.core.member.MemoryMemberRepository;
-import com.study.core.order.OrderService;
-import com.study.core.order.OrderServiceImpl;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
 public class AppConfig {
-
-    @Bean
-    public MemberService memberService() {
-        return new MemberServiceImpl(getMemberRepository());
-    }
-
-    @Bean
-    public OrderService orderService() {
-        return new OrderServiceImpl(getMemberRepository(), getDiscountPolicy());
-    }
-
-    @Bean
-    public MemoryMemberRepository getMemberRepository() {
-        return new MemoryMemberRepository();
-    }
-
-    @Bean
-    public DiscountPolicy getDiscountPolicy() {
-        return new FixDiscountPolicy();
-    }
 }
