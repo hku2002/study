90349d5f139e5dba5948af446dfd0a40b4a63acb
Hwang <hku2002@hanmail.net>
2023-04-26 23:19:33 +0900

    feat: 이메일, 비밀번호 추가

--- design-pattern/src/main/java/com/study/design/pattern/common/domain/User.java
+++ design-pattern/src/main/java/com/study/design/pattern/common/domain/User.java
@@ -21,13 +21,19 @@ public class User {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
+
+    private String email;
+
+    private String password;
     private String username;
     private String phoneNumber;
     private String grade;
 
     @Builder
-    public User(Long id, String username, String phoneNumber, String grade) {
+    public User(Long id, String email, String password, String username, String phoneNumber, String grade) {
         this.id = id;
+        this.email = email;
+        this.password = password;
         this.username = username;
         this.phoneNumber = phoneNumber;
         this.grade = grade;
