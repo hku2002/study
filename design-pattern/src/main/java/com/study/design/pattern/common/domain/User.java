2d32d2b0592daff93df874ac113686e2c7536561
Hwang <hku2002@hanmail.net>
2023-04-26 23:51:48 +0900

    feat: email 컬럼 unique 제약 조건 추가

--- design-pattern/src/main/java/com/study/design/pattern/common/domain/User.java
+++ design-pattern/src/main/java/com/study/design/pattern/common/domain/User.java
@@ -4,17 +4,15 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 import static javax.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
 
 @Getter
 @Entity
-@Table(name = "USERS")
+@Table(name = "USERS", uniqueConstraints =
+@UniqueConstraint(name = "uni_email", columnNames = { "email" }))
 @NoArgsConstructor(access = PROTECTED)
 public class User {
 
