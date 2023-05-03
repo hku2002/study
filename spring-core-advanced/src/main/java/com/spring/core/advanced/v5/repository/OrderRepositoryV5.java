716f845b1ef12eaad138b13317291fb0520aa60d
Hwang <hku2002@hanmail.net>
2023-05-04 01:04:15 +0900

    feat: 서비스 repository 템플릿 콜백 패턴 추가

--- spring-core-advanced/src/main/java/com/spring/core/advanced/v5/repository/OrderRepositoryV5.java
+++ spring-core-advanced/src/main/java/com/spring/core/advanced/v5/repository/OrderRepositoryV5.java
@@ -1,28 +1,30 @@
 package com.spring.core.advanced.v5.repository;
 
+import com.spring.core.advanced.trace.callback.TraceCallback;
+import com.spring.core.advanced.trace.callback.TraceTemplate;
 import com.spring.core.advanced.trace.logtrace.LogTrace;
 import com.spring.core.advanced.trace.template.AbstractTemplate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@RequiredArgsConstructor
 public class OrderRepositoryV5 {
 
-    private final LogTrace trace;
+    private final TraceTemplate traceTemplate;
+
+    public OrderRepositoryV5(LogTrace logTrace) {
+        this.traceTemplate = new TraceTemplate(logTrace);
+    }
 
     public void save(String itemId) {
-        AbstractTemplate<Void> template = new AbstractTemplate<>(trace) {
-            @Override
-            protected Void call() {
-                if ("ex".equals(itemId)) {
-                    throw new IllegalStateException("예외발생");
-                }
-                sleep(1000);
-                return null;
+
+        traceTemplate.execute("OrderRepository.save()", () -> {
+            if ("ex".equals(itemId)) {
+                throw new IllegalStateException("예외발생");
             }
-        };
-        template.execute("OrderRepository.save()");
+            sleep(1000);
+            return null;
+        });
     }
 
     private void sleep(int mills) {
--- spring-core-advanced/src/main/java/com/spring/core/advanced/v5/service/OrderServiceV5.java
+++ spring-core-advanced/src/main/java/com/spring/core/advanced/v5/service/OrderServiceV5.java
@@ -1,26 +1,28 @@
 package com.spring.core.advanced.v5.service;
 
+import com.spring.core.advanced.trace.callback.TraceCallback;
+import com.spring.core.advanced.trace.callback.TraceTemplate;
 import com.spring.core.advanced.trace.logtrace.LogTrace;
 import com.spring.core.advanced.trace.template.AbstractTemplate;
-import com.spring.core.advanced.v4.repository.OrderRepositoryV4;
-import lombok.RequiredArgsConstructor;
+import com.spring.core.advanced.v5.repository.OrderRepositoryV5;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class OrderServiceV5 {
 
-    private final OrderRepositoryV4 orderRepository;
-    private final LogTrace trace;
+    private final OrderRepositoryV5 orderRepository;
+    private final TraceTemplate traceTemplate;
+
+    public OrderServiceV5(OrderRepositoryV5 orderRepository, LogTrace logTrace) {
+        this.orderRepository = orderRepository;
+        this.traceTemplate = new TraceTemplate(logTrace);
+    }
 
     public void orderItem(String itemId) {
-        AbstractTemplate<Void> template = new AbstractTemplate<>(trace) {
-            @Override
-            protected Void call() {
-                orderRepository.save(itemId);
-                return null;
-            }
-        };
-        template.execute("OrderService.orderItem()");
+
+        traceTemplate.execute("OrderService.orderItem()", () -> {
+            orderRepository.save(itemId);
+            return null;
+        });
     }
 }
